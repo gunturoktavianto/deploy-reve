e4828fe5b38305274b169ea91e5e339bcd21c29d
Guntur Oktavianto Nugroho <gunturoktaviant@gmail.com>
2023-10-20 06:53:38 +0700

    bug fixed

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    API_URL: "http://localhost:3000",
-    DB_LOCAL_URI: "mongodb+srv://ReveDB:Inibuatrevedb@reve.s1nbcek.mongodb.net/",
-    DB_URI: "mongodb+srv://ReveDB:Inibuatrevedb@reve.s1nbcek.mongodb.net/",
-  },
+    experimental: {
+        appDir: true,
+    },
+    env: {
+        API_URL:
+            "https://deploy-reve-k5cz51j48-buatgdrive43-gmailcom.vercel.app/",
+        DB_LOCAL_URI:
+            "mongodb+srv://ReveDB:Inibuatrevedb@reve.s1nbcek.mongodb.net/",
+        DB_URI: "mongodb+srv://ReveDB:Inibuatrevedb@reve.s1nbcek.mongodb.net/",
+    },
 };
 
 module.exports = nextConfig;
