a9e24ed9c50672fdc77d0aaae342d1ce721638be
Guntur Oktavianto Nugroho <gunturoktaviant@gmail.com>
2023-10-20 07:03:47 +0700

    fixed domain

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
         appDir: true,
     },
     env: {
-        API_URL:
-            "https://deploy-reve-k5cz51j48-buatgdrive43-gmailcom.vercel.app/",
+        API_URL: "https://reveindonesia.site",
         DB_LOCAL_URI:
             "mongodb+srv://ReveDB:Inibuatrevedb@reve.s1nbcek.mongodb.net/",
         DB_URI: "mongodb+srv://ReveDB:Inibuatrevedb@reve.s1nbcek.mongodb.net/",
